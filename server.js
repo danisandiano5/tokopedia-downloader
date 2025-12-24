e4097860401f41ebde3254f9fdaccdaed3ffbea4
danisandiano5 <danisandiano5@gmail.com>
2025-12-24 22:41:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Tokopedia Downloader API is running");
-});
-
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
